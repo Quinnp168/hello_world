f93e70a1b01b0a6024d76a01c220aa4f6c7e138b
Quinnp168 <59508579+Quinnp168@users.noreply.github.com>
2022-11-14 07:40:25 -0800

    Update array_sort_inefficient.c

--- array_sort_inefficient.c
+++ array_sort_inefficient.c
@@ -2,26 +2,28 @@
 
 void sort_string(int s[], int len){
 	for( int i = 0; i < len; i += 1 ){
-		int s1 = s[0];
-		int s2 = s[1];
-		if( s[0] > s[1]){
-			s[0] = s2;
-			s[1] = s1;
+		for( int j = 0; j < len - 1; j++ ){
+		if( s[j] > s[j + 1]){
+		    int t = s[j];
+		    s[j] = s[j + 1];
+		    s[j + 1] = t;
 		}
-		if( s[1] > s[2]){
-			s2 = s[1];
-			int s3 = s[2];
-			s[1]= s3;
-			s[2] = s2;
 		}
 	}
 }
 
 int main(void) {
 	// your code goes here
-	int s[] = { 7, 4, 3 };
-	printf("%d %d %d\n", s[0], s[1], s[2]);
-	sort_string(s, 3);
-	printf("%d %d %d\n", s[0], s[1], s[2]);
+	int s[] = { 7, 4, 3 , 8, 10, 5,24,87,-32,50};
+	int len = sizeof s / sizeof s[0];
+	for( int i = 0; i < len; i++ ){
+	    printf("%d ", s[i]);
+	}
+	putchar('\n');
+	sort_string(s, len);
+	for( int i = 0; i < len; i++ ){
+	    printf("%d ", s[i]);
+	}
+	
 	return 0;
 }
