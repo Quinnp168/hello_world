5976f33844c2680f212de0726e26d283fbf7e0eb
Quinnp168 <59508579+Quinnp168@users.noreply.github.com>
2022-11-16 10:25:10 -0800

    Update bubble_sort.c
    comments

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 void sort_string(int s[], int len){
+	//loops for all numbers
 	for( int i = 0; i < len; i += 1 ){
 	    	for( int j = 0; j < len - 1 - i; j++ ){
+			//changes order
 			if( s[j] > s[j + 1]){
 		    		int t = s[j];
 		    		s[j] = s[j + 1];
@@ -14,7 +16,7 @@ void sort_string(int s[], int len){
 
 int main(void) {
 	// your code goes here
-	int s[] = { 7, 4, 3 , 8, 10, 5,24,87,-32,50};
+	int s[] = { 7, 4, 3, 8, 10, 5, 24, 87, -32, 50};
 	int len = sizeof s / sizeof s[0];
 	// printing unsorted number
 	for( int i = 0; i < len; i++ ){
