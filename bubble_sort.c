24a37a5b622e2da3484f370d2a4f7e61e427e6b6
Quinnp168 <59508579+Quinnp168@users.noreply.github.com>
2022-11-16 10:09:50 -0800

    Update and rename array_sort_inefficient.c to bubble_sort.c
    add comments and indentation.

--- bubble_sort.c
+++ bubble_sort.c
@@ -2,12 +2,12 @@
 
 void sort_string(int s[], int len){
 	for( int i = 0; i < len; i += 1 ){
-		for( int j = 0; j < len - 1 - i; j++ ){
-		if( s[j] > s[j + 1]){
-		    int t = s[j];
-		    s[j] = s[j + 1];
-		    s[j + 1] = t;
-		}
+	    	for( int j = 0; j < len - 1 - i; j++ ){
+			if( s[j] > s[j + 1]){
+		    		int t = s[j];
+		    		s[j] = s[j + 1];
+		   		s[j + 1] = t;
+			}
 		}
 	}
 }
@@ -16,11 +16,13 @@ int main(void) {
 	// your code goes here
 	int s[] = { 7, 4, 3 , 8, 10, 5,24,87,-32,50};
 	int len = sizeof s / sizeof s[0];
+	// printing unsorted number
 	for( int i = 0; i < len; i++ ){
 	    printf("%d ", s[i]);
 	}
 	putchar('\n');
 	sort_string(s, len);
+	//printing sorted numbers
 	for( int i = 0; i < len; i++ ){
 	    printf("%d ", s[i]);
 	}
